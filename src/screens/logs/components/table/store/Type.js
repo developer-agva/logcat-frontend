c7986af6822334c8592bcad7a4653babf6824f21
shivprakash121 <shivprakash@agvahealthtech.com>
2024-10-29 14:23:45 +0530

    E:Support and service engineer module with graph data

---
+++ src/screens/logs/components/table/store/Type.js
@@ -0,0 +1,21 @@
+export const SINGLE_CHECKBOX_SELECTION = "SINGLE_CHECKBOX_SELECTION";
+export const ALL_CHECkBOX = "ALL_CHECkBOX";
+// ==================
+export const SELECT_PAGE_NO = "SELECT_PAGE_NO";
+// ==================
+export const DATE_SELECTION = "DATE_SELECTION";
+export const STATUS_SELECTION = "STATUS_SELECTION";
+export const RECORD_PER_PAGE_SECTION = "RECORD_PER_PAGE_SECTION";
+// ==================
+export const COUNT_PER_PAGE = "COUNT_PER_PAGE";
+// ====================
+export const LOGTYPE = "LOGTYPE";
+// ===================
+export const SORT_ICON_FILTER = "SORT_ICON_FILTER";
+// ===================
+export const RECORDS = "RECORDS";
+export const ACTIVE_RECORDS = "ACTIVE_RECORDS";
+// ===================
+export const DATE = "DATE";
+// ===================
+export const SEARCH_FIELD = "SEARCH_FIELD";
